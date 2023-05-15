952ea5cc9d5c3bbfb47d746a0794176105fd77e2
DESKTOP-8UPBPUJ\학생 <phantom9512@naver.com>
2023-05-15 17:17:58 +0900

    no message

---
+++ js/0515-1.js
@@ -0,0 +1,304 @@
+//형변환
+//String() 문자로 변환
+//Number() 숫자로 변환
+//Boolean() 불리언형으로 변환
+// perseInt() 정수로 변환
+
+// const 수학=Number(prompt("수학몇점?"));
+// const 영어=Number(prompt("영어몇점?"));
+// const 결과=(수학+영어)/2;
+// console.log(결과);
+
+var 수학1 = 80;
+var 영어1 = 100;
+var 결과1 = (수학1 + 영어1) / 2;
+console.log(결과1);
+
+console.log(typeof 수학1);
+var 수학 = String(수학1);
+console.log(typeof 수학);
+
+var num = '1000'; //문자
+var num1 = 1000 //숫자
+console.log(num / 2);  //자동형변환 '1000'/2
+console.log(num1 / 2);
+
+// 문자형을 나누면 숫자형으로 자동형변환이 이루어진다.
+
+console.log(Boolean(1));  //true
+console.log(Boolean(0));  //false
+console.log(Boolean("welcome"));  //true
+console.log(Boolean(""));  //false 빈 문자열
+console.log(Boolean(null));        //false
+console.log(Boolean(undefined));   //false
+console.log(Boolean(NaN));        //false
+
+console.log(Boolean("0"));  // true
+console.log(Boolean(' '));  // 공백 true
+
+//연산자
+var num = 2 ** 3;      //거듭제곱
+console.log(num);
+
+// 복합대입연산자
+var num1 = 10;
+num1 = num1 * 5;
+console.log(num1);
+
+num1 += 10;
+console.log(num1);
+num1 -= 5;
+num1 *= 2;
+num1 /= 2;  //num1=num/2
+num %= 2; // num=num% 나머지
+
+console.time("시작");
+num1 = num1 + 10;
+console.timeEnd("시작");
+
+console.time("시작");
+num1 -= 10;
+console.timeEnd("시작");
+
+//동등연산다 , 동치연산자
+// 값만 비교, 값과 타입을 비교
+
+var a = 1;
+var b = '1';
+console.log(a == b);       //true
+console.log(a === b);      //false
+
+// 비교연산자
+//  >   <  >= <= !=(not)
+
+var age = 19;
+
+//조건문
+if (age > 19) {
+    console.log("환영합니다.");
+}
+if (age < 19) {
+    console.log("안녕히가세요")
+}
+
+age = 20;
+
+//if,else,else if 다중조건문
+if (age > 10) {
+    console.log("환영합니다.")
+} else if (age == 19) {
+    console.log("19살입니다.")
+} else {
+    console.log("미성년입니다.")
+}
+
+//사과있으면 사과사오고, 양파, 마늘도 사와
+//사과없으면 나머지 시장을 안본다
+
+// 논리연산자, 비교연산자,삼향연산자,짧은조건연산자
+// AND(&),OR(||),!(NOT)
+
+var a = 100;
+var b = 200;
+var c = true;
+var d = false;
+
+console.log(c && d);
+console.log(c || d);
+
+// OR는 하나만 참이면 결과가 참이다.
+// AND는 하나만 거짓이면 결과가 거짓이다. 모두참- >참
+// 스티브잡스는 한국인이거나 남자이다 참->
+// 스티브잡스는 한국인이고 남자이다->거짓
+
+console.log(a > b);
+console.log(!(a > b));
+
+var 성별 = "여성";
+var 이름 = "제인";
+var 성인 = true;
+
+// 남자이고, 이름이 마이크이거나 성인이면 통과
+if (성별 == "남성" && (이름 == "마이크" || 성인 == true)) {
+    console.log = ("통과");
+} else {
+    console.log("입장불가");
+}
+
+// 연산자우선순위 (a+b)*c a+b*c
+
+// 삼향연산자  ?를 이용한 조건문
+// 조건식 ? 실행문1: 실행문
+// 조건식이 참이면 실행문1이 실행되고 거짓이면 실행문2가 실행된다.
+
+var html = "html5"
+
+if (html == "html5") {
+    console.log("javascript");
+} else {
+    console.log("css");
+}
+
+var result = html = "html" ? "javascript" : "css";
+//조건                참        거짓
+
+console.log(result);
+
+// 짧은 조건문
+// A || B       A가 거짓일때 B가 실행된다.
+// A && B       A가 참이면 B가 실행된다.
+
+var str1 = "welcome";
+console.log(true || str1);    //true
+console.log(false || str1);    //welcome
+console.log(true && str1);     //welcome
+console.log(false && str1);       //true
+
+// 함수선언
+function 과일(name, count) {
+    var count = count || 1;
+    console.log(name + "이" + count + "개입니다.");
+}
+
+//함수 호출
+과일("수박");
+과일("수박", 5);
+// 오버로딩
+
+// if(count==undefined){
+//     count=1;
+// }
+
+
+//반복문
+//동일한 작업을 여러번 반복할때 사용
+
+// debugger;
+
+for (var i = 0; i < 10; i++); {
+    console.log(i); //i ->0 ~ 9
+}
+console.log("for문끝난 후: " + i);
+
+// 디버깅 모드  ->f5 ->f11
+var i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+
+
+// while(true){
+//     // 무한루프...
+// }
+// for(;;){
+//  무한루프
+// }
+
+var input = 10;
+
+switch (input % 2) {
+    case 0: console.log("짝수입니다."); break;
+    case 1: console.log("홀수입니다."); break;
+    default: console.log("숫자가 아닙니다."); break;
+}
+
+// 예시2
+var 국어 = 90;
+
+switch (true) {
+    case 국어 >= 90:
+        console.log("A학점입니다..");
+        break;
+    case 국어 >= 80:
+        console.log("홀수입니다.");
+        break;
+
+    case 국어 >= 70:
+        console.log("숫자가 아닙니다.");
+            ; break;
+    default:
+        console.log("F학점입니다.");
+        break;
+}
+
+// prompt로 네이버, 다음, 네이트, 구글을 입력하면
+// 해당사이트로 링크되게 해보자. switch문 활용
+
+var site=prompt("가고싶은 포털사이트?")
+var url;
+
+switch(site){
+    case "네이버":
+    url="http://www.naver.com"; break;
+    case "다음":
+    url="http://www.daum.net"; break;
+    case "구글":
+    url="http://www.google.com"; break;
+    case "네이트":
+    url="http://www.nate.com"; break;
+    
+    default:
+        console.log("없는 사이트 입니다."); break;
+        
+}
+if(url) location.href=url;
+// 한줄은 중괄호 생략가능
+
+var x,y;
+var input;
+
+input=prompt("정수를 입력하시오","정수로");
+x = parseInt(input);
+
+input=prompt("정수를 입력하시오","정수로");
+y = parseInt(input);
+
+document.write(x + y + "<br>");
+
+function calc() {
+
+    var x = document.getElementById("x").value;
+
+    var y = document.getElementById("y").value;
+    var sum;
+
+    sum = parseInt(x) + parseInt(y);
+    document.getElementById("sum").value = sum;
+}
+function fune(){
+    e=document.getElementById("test");
+    e.style.color="red";
+}
+if (time<12)
+{
+	greeting="Good Morning!";
+}
+if (time<12){
+    msg="good Morning";
+}else{
+    msg="good afternoon";
+}
+var msg="";1
+var time=new Date().getHours();
+if (time<12){
+    msg="good morning";
+
+}else if(time<18){
+    msg="good afternoon"
+}
+else{
+    msg="good evening";
+}
+alert(msg);
+
+var grade = prompt("성적을 입력하시오.");
+switch(grade){
+    case 'A' :alert("잘했어요");break;
+    case 'B' :alert("좋은 점수군요");
+    case 'C' :alert("괜찮은 점수군요");
+    case 'D' :alert("좀더 노력하세요");
+    case 'E' :alert("다음학기 수강하세요");
+    case 'F' :alert("알수없는 학점입니다.");
+}
--- js/0515.js
+++ js/0515.js
@@ -1,43 +1,43 @@
 // javascript basic
 
 // 변수--->메모리공간에 하나의 저장이 생긴다.
-var name="홍길동";
+var name = "홍길동";
 var age;
-age=30;
+age = 30;
 
 // 변수는 선언을 하고,
 // 그 변수에 값을 할당해야한다.
 //할당하지 않으면 undefined;
 document.write("<h2>name<h2>");
-document.write("<h2>"+name+"<h2>");
-document.write("<h2>"+age+"살<h2>");
+document.write("<h2>" + name + "<h2>");
+document.write("<h2>" + age + "살<h2>");
 
 document.write(`<h2>${name}은 ${age}살입니다.<h2>`);
 // ` 백틱 esc 밑에
 // 문자는 따옴표, 쌍따옴표, 훗따옴표로 감싼다.
-var age=40;
+var age = 40;
 
 var 이름;  // undefined
 document.write(이름); // undefined 정의가 안되었음
-이름="장동건";
+이름 = "장동건";
 document.write(이름);
-이름="원빈";
+이름 = "원빈";
 document.write(이름);
 console.log("내 이름은 " + 이름 + " 입니다.");
 
 
 //let 과 const
 
-let name1="Mike";
+let name1 = "Mike";
 console.log(name1);
-name1="Tomson";
+name1 = "Tomson";
 console.log(name1);
-let name2="마이클";
+let name2 = "마이클";
 //let으로 선언한 변수명은 중복사용할 수 없다.
 //let과 var는 스코프 scope- 렉시컬환경, 영역
 
 //const 상수 : 절대로 ㅏ바뀌지 않는 상수
-const 생일="2010-10-01"
+const 생일 = "2010-10-01"
 console.log(생일);
 // 생일="2010-10-10";
 console.log(생일);
@@ -45,7 +45,7 @@ console.log(생일);
 
 // var 05name="홍길동"; 
 
-var let="let me";
+var let = "let me";
 console.log(let);
 // var for="welcome"
 
@@ -61,39 +61,39 @@ console.log(let);
 // 변수명은 읽기 쉽고 이해할 수 있게 선언한다.
 // 문자와숫자 , &와  _만 사용가능하다.
 
-var BIRTHDAY=new Date();
+var BIRTHDAY = new Date();
 console.log(BIRTHDAY.toLocaleTimeString());
 
-let $aaa_=300;
+let $aaa_ = 300;
 
-let msg="i'm a boy";
+let msg = "i'm a boy";
 console.log(msg);
-let msg2="i\m a boy";
+let msg2 = "i\m a boy";
 console.log(msg);
-let msg3=`My name is ${name}`;
+let msg3 = `My name is ${name}`;
 console.log(msg3);
-let msg4=`나는 ${30+1}살 입니다.`;
+let msg4 = `나는 ${30 + 1}살 입니다.`;
 console.log(msg4);
 
 // 숫자 Number, 문자 string
 // 더하기, 뺴기, 곱하기 ,나누기, 나머지
 //    +     -      *       /       %
 
-console.log(name/2); // Nan, Not a number
-console.log(이름*2);
-console.log(이름+10);// +문자와 연결기능
-console.log(이름-10);// NAN
+console.log(name / 2); // Nan, Not a number
+console.log(이름 * 2);
+console.log(이름 + 10);// +문자와 연결기능
+console.log(이름 - 10);// NAN
 
 //  + 더하기 기능, 문자와 연결연산자 기능
 
 //Bollean 불리언 - 참,거짓,true,false
 // 불대수-논리학
-let a=true;
-let b=false;
-console.log(a==b);
+let a = true;
+let b = false;
+console.log(a == b);
 // == 등가연산자, ===등치연산자(일치연산자)
-console.log(10>5); //true
-console.log(name=="홍길동");
+console.log(10 > 5); //true
+console.log(name == "홍길동");
 
 // 프로그래머 아내
 // 남편 프로그래머
@@ -107,13 +107,13 @@ console.log(typeof 3)
 
 // var age=null;  -> object 객체
 
-function add(){  }       //함수
+function add() { }       //함수
 console.log(typeof add);
-var 고양이={  }      //객체
+var 고양이 = {}      //객체
 console.log(typeof 고양이);
 
 // 숫자+문자 = 문자
-console.log(100+"hi");
+console.log(100 + "hi");
 
 // 문장과 표현식
 // statement와 expression
@@ -126,63 +126,70 @@ console.log(100+"hi");
 // pormt  입력받는 창
 // confirm  확인받는 창
 
-var 별명=prompt("별명을 입력하세요","한글로");
+var 별명 = prompt("별명을 입력하세요", "한글로");
 console.log("환영합니다." + 별명 + "님");
 
-var 성인=confirm("당신은 성인입니다?");
+var 성인 = confirm("당신은 성인입니다?");
 //is adult
 // 확인 -> true , 취소 ->false
 //조건문
 
-if(성인==true){
+if (성인 == true) {
     console.log("당신은 성인이군요");
-}else{
+} else {
     console.log("당신은 아직 미성년자이군요");
-} 
+}
 
 // = 은 대입연산자
 // a = 3; // a 라는 변수 3을 대입해라
 // ==, ===
 
-var x="Hellow world"
+var x = "Hellow world"
 alert(x);
 
 var s;
-s=100;
+s = 100;
 document.write(s + "<br>");
 
-s="홍길동";
+s = "홍길동";
 document.write(s + "<br>")
 
-var s ="Hellow world"
-var t ="How are you" + " today?";
+var s = "Hellow world"
+var t = "How are you" + " today?";
 
 document.write(s + "<br> ");
 document.write(t + "<br> ");
 document.write(s.toUpperCase() + "<br> ");
 
-var myCar = {model: "bmz", color:"red", hp:100};
-document.write(myCar.model+"<br>");
-document.write(myCar.color+"<br>");
-document.write(myCar.hp+"<br>");
+var myCar = { model: "bmz", color: "red", hp: 100 };
+document.write(myCar.model + "<br>");
+document.write(myCar.color + "<br>");
+document.write(myCar.hp + "<br>");
 
-var age=prompt("나이를 입력하세요","만 나이로 입력합니다.");
+var age = prompt("나이를 입력하세요", "만 나이로 입력합니다.");
 
-var x,y;
+var x, y;
 var input;
 
-input = prompt("정수를 입력하시오","정수로");
-x= parseInt(input);
+input = prompt("정수를 입력하시오", "정수로");
+x = parseInt(input);
 
-input = prompt("정수를 입력하시오","정수로");
+input = prompt("정수를 입력하시오", "정수로");
 y = parseInt(input);
 
-document.write(x + y +"<br>")
+document.write(x + y + "<br>")
 
 var s;
 
 s = 100;
 document.write(s + "<br>");
 
-s ="홍길동"
+s = "홍길동"
 document.write(s + "<br>");
+
+function 호랑이(){
+    console.log("나는 호랑이")
+}
+var 호랑=new 호랑이();
+console.log(JSON.stringify(호랑이));
+console.log(호랑);
